4e486589263a5700503217c8516c6887bb1d1a92
Daniel Schulz <schulz@sensape.com>
2023-11-12 00:34:30 +0100

    feat: tools and clients

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,9 @@ const techstack: Tech[] = [
   {
     name: 'React',
     url: "https://reactjs.org/",
-  }, {
+  },
+  { name: 'Remix', url: 'https://remix.run' },
+  {
     name: 'TypeScript',
     url: "https://www.typescriptlang.org/",
   }, {
@@ -33,9 +35,84 @@ const techstack: Tech[] = [
   }, {
     name: 'Git',
     url: "https://git-scm.com/",
+  }, {
+    name: "MongoDB",
+    url: "https://www.mongodb.com/"
   }]
 
+const tools: Tech[] = [
+  {
+    name: 'VS Code',
+    url: "https://code.visualstudio.com/",
+  }, {
+    name: 'ChatGPT',
+    url: "https://openai.com/chatgpt"
+  }, {
+    "name": "Github Copilot",
+    "url": "https://copilot.github.com/"
+  }, {
+    name: "Supabase",
+    url: "https://supabase.io/"
+  }, {
+    name: "FeathersJS", url: "https://feathersjs.com/"
+  }, {
+    name: 'iTerm2',
+    url: "https://iterm2.com/",
+  }, {
+    name: "Rest APIs",
+    url: "https://restfulapi.net/"
+  }, {
+    name: "Gatsby",
+    url: "https://www.gatsbyjs.com/"
+  },
+  {
+    name: "NextJS",
+    url: "https://nextjs.org/"
+  }, {
+    name: "TailwindCSS",
+    url: "https://tailwindcss.com/"
+  },
+  { name: 'ESLint', url: 'https://eslint.org/' },
+  { name: 'Prettier', url: 'https://prettier.io/' },
+  { name: 'Vite', url: 'https://vitejs.dev/' },
+  { name: 'Jest', url: 'https://jestjs.io/' },
+  { name: 'Netlify', url: 'https://netlify.com' },
+  { name: 'FlyIO', url: 'https://fly.io' },
+]
 
+const clients: Tech[] = [
+  {
+    name: 'FC Bayern München',
+    url: 'https://www.sensape.com/projects/ar-photos-with-the-fc-bayern-stars'
+  },
+  {
+    name: 'Lavazza',
+    url: 'https://lavazza.de'
+  },
+  {
+    name: 'Disney',
+    url: 'https://elemental-quiz.de/'
+  }, {
+    name: 'Mazda',
+    url: 'https://mazda.de'
+  }, {
+    name: 'Rheem',
+    url: 'https://www.sensape.com/projects/rheem-digital-sales-process-at-the-pos'
+  },
+  {
+    name: 'Paris Saint-Germain',
+    url: 'https://www.sensape.com/projects/virtual-selfies-with-psg-players'
+  }, {
+    name: 'KTM',
+    url: 'https://www.sensape.com/projects/selfies-with-ktm-drivers-for-traffic-and-sales-boost'
+  }, {
+    'name': 'Coca-Cola',
+    'url': 'https://coca-cola.com'
+  }, {
+    name: 'RTL',
+    url: 'https://www.sensape.com/projects/rtl-uses-interactive-ar-solution-to-promote-the-new-season'
+  }
+]
 
 function App() {
 
@@ -53,7 +130,7 @@ function App() {
         </h2>
         <ul className='techstack'>
           {techstack.map((tech) => (
-            <li className='techstackitem' key={tech.name}>
+            <li className='pill techstackitem' key={tech.name}>
               <a className='techstacklink' href={tech.url} target='_blank' rel="noopener noreferer">
                 {tech.name}
               </a>
@@ -95,6 +172,38 @@ function App() {
           </a>
           , where I currently work as a web developer specializing in frontend. JavaScript, Node, and React are my most trusted companions in this endeavor.
         </p>
+        <h2>
+          Tools i &#128525; to use:
+        </h2>
+        <ul className='techstack'>
+          {tools.map(x => {
+            return <li className='pill toolsitem' key={x.name}>
+              <a className='techstacklink' href={x.url} target='_blank' rel="noopener noreferer">{x.name}</a>
+            </li>
+          })}
+        </ul>
+        <h2>
+          Clients made happy:
+        </h2>
+        <ul className='techstack'>
+          {clients.map(x => {
+            return <li className='pill clientitem' key={x.name}>
+              <a className='techstacklink' href={x.url} target='_blank' rel="noopener noreferer">{x.name}</a>
+            </li>
+          })}
+        </ul>
+        <h2>Contact:</h2>
+        <div>
+          Email: <a href="mailto:d.schulz@hello-sunshine.dev">
+            d.schulz [at] hello-sunshine.dev
+          </a>
+        </div>
+        <div>
+          Phone: <a href="tel:+4917630374218">0176-30374218</a>
+        </div>
+        <div>
+          LinkedIn: <a href="https://www.linkedin.com/in/daniel-schulz-226370109/" rel="noopener noreferer" target="_blank">show profile</a>
+        </div>
       </main>
     </>
   )
